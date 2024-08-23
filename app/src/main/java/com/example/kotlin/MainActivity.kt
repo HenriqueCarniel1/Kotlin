d67e64fbf24693258fc3bbf8bd38ff54baef17dc
Gustavo Henrique Queiroz Carniel <henriquecarniel@dinfor.uniceub>
2024-08-23 09:33:48 -0300

    first commit

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin
 
 import android.os.Bundle
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,17 +9,33 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Configuração para permitir o uso de "edge-to-edge"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val helloWorldTextView: TextView = findViewById<TextView>(R.id.hello);
-        helloWorldTextView.text = "Gustavo Henrique";
+        // Encontrar o TextView e o Switch no layout
+        val helloWorldTextView: TextView = findViewById(R.id.hello)
+        val switch: Switch = findViewById(R.id.switch1)
+
+        // Definir o texto inicial do TextView
+        helloWorldTextView.text = "Hello World"
+
+        // Configurar um listener para o Switch
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                helloWorldTextView.text = "Gustavo Henrique"
+            } else {
+                helloWorldTextView.text = "Hello World"
+            }
+        }
     }
 }
